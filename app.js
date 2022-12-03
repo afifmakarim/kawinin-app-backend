5d98abf4da51ca36cfba552266e5c7113ddbd7fd
unknown <afifmakarim88@gmail.com>
2022-12-03 21:03:42 +0700

    fix config new

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+// app.use("/", indexRouter);
 app.use("/api/v1", usersRouter);
 
 // const weddingList = require("./models/weddingList.model");
